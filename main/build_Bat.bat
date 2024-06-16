4ac30dbe338079497d5d24911916c05317bf57b6
Nodang <noj7413@naver.com>
2024-06-16 22:34:18 +0900

    8

--- main/build_Bat.bat
+++ main/build_Bat.bat
@@ -1,10 +1,10 @@
-set PATH_BAT=..\Compiler
+set PATH_BAT=../Compiler
 set TARGET_BAT=a_hara
 
 cls
 
-%PATH_BAT%\bin\make.exe clean
-%PATH_BAT%\bin\make.exe all
-%PATH_BAT%\bin\hex2000.exe %TARGET_BAT%.out -o %TARGET_BAT%.hex -map %TARGET_BAT%.map -i -datawidth 16 -memwidth 16 -romwidth 16 
+%PATH_BAT%/bin/make.exe clean
+%PATH_BAT%/bin/make.exe all
+%PATH_BAT%/bin/hex2000.exe %TARGET_BAT%.out -o %TARGET_BAT%.hex -map %TARGET_BAT%.map -i -datawidth 16 -memwidth 16 -romwidth 16 
 
 @echo Compile Time %DATE% %TIME%
