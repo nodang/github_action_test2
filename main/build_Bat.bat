afa249b93cbdab3f2fceb6380765a1afc2c97a71
노호진 <noj7413@naver.com>
2024-06-16 19:30:15 +0900

    Update build_Bat.bat

--- main/build_Bat.bat
+++ main/build_Bat.bat
@@ -3,8 +3,8 @@ set TARGET_BAT=a_hara
 
 cls
 
-%PATH_BAT%\bin\make clean
-%PATH_BAT%\bin\make all
-%PATH_BAT%\bin\hex2000 %TARGET_BAT%.out -o %TARGET_BAT%.hex -map %TARGET_BAT%.map -i -datawidth 16 -memwidth 16 -romwidth 16 
+%PATH_BAT%\bin\make.exe clean
+%PATH_BAT%\bin\make.exe all
+%PATH_BAT%\bin\hex2000.exe %TARGET_BAT%.out -o %TARGET_BAT%.hex -map %TARGET_BAT%.map -i -datawidth 16 -memwidth 16 -romwidth 16 
 
 @echo Compile Time %DATE% %TIME%
