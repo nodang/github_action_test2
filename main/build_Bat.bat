0feddadb6b56e9c0883a8658bd135bc061816b53
Nodang <noj7413@naver.com>
2024-08-10 11:35:10 +0900

    change / -> \
    Signed-off-by: Nodang <noj7413@naver.com>

--- main/build_Bat.bat
+++ main/build_Bat.bat
@@ -1,10 +1,22 @@
-set PATH_BAT=../Compiler
+
+@REM set PATH_BAT=../Compiler
+@REM set TARGET_BAT=a_hara
+
+@REM cls
+
+@REM %PATH_BAT%/bin/make.exe clean
+@REM %PATH_BAT%/bin/make.exe all
+@REM %PATH_BAT%/bin/hex2000.exe %TARGET_BAT%.out -o %TARGET_BAT%.hex -map %TARGET_BAT%.map -i -datawidth 16 -memwidth 16 -romwidth 16 
+
+@REM @echo Compile Time %DATE% %TIME%
+
+set PATH_BAT=..\Compiler
 set TARGET_BAT=a_hara
 
 cls
 
-%PATH_BAT%/bin/make.exe clean
-%PATH_BAT%/bin/make.exe all
-%PATH_BAT%/bin/hex2000.exe %TARGET_BAT%.out -o %TARGET_BAT%.hex -map %TARGET_BAT%.map -i -datawidth 16 -memwidth 16 -romwidth 16 
+%PATH_BAT%\bin\make clean
+%PATH_BAT%\bin\make all
+%PATH_BAT%\bin\hex2000 %TARGET_BAT%.out -o %TARGET_BAT%.hex -map %TARGET_BAT%.map -i -datawidth 16 -memwidth 16 -romwidth 16 
 
 @echo Compile Time %DATE% %TIME%
